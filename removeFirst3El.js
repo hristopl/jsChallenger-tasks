747779f51d468429d90658ee9edb8bccff09711a
strypes-lyubox <102239655+strypes-lyubox@users.noreply.github.com>
2022-04-28 12:15:38 +0300

    Update removeFirst3El.js
    Мисли и за тия работи

--- removeFirst3El.js
+++ removeFirst3El.js
@@ -2,11 +2,7 @@
 // Remove the first 3 elements of 'a'
 // Return the result
 
-const getNthElement = a => {
-
-
-return a.splice(0,3)
-}
+const getNthElement = a => a.splice(0,3)
 
 const a = [5,4,3,2,1,0]
 
